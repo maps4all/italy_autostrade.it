5669e24f9844b42904b82d73d3b079020ba84c24
maps4all <maps4all007@yandex.com>
2017-08-30 16:43:47 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,10 @@
+{
+    "moduleId" : "maps4all/italy_autostrade.it",
+    "type" : "Website",
+    "name" : "Autostrade.it",
+    "uri" : "httü://www.autostrade.it/autostrade-gis/news.do",
+    "startTrigger" : "Dati aggiornati",
+    "endTrigger" : "Rete Autostrade",
+    "startTag" : "h3",
+    "endTag" : ""
+}
